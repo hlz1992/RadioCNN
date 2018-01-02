2a553c1083f7957201a5694f5d86a9e894ee8829
Longozhuang He <helongzhuang@126.com>
2018-01-02 23:18:51 +0800

    Number of training steps updated to 20000

--- radioCNN.py
+++ radioCNN.py
@@ -237,7 +237,7 @@ def main(argv=None):
             SNR = SNR, 
             output_accuracy=False, 
             MAX_PILOT_NUM=50000,
-            TRAINING_STEPS=10000
+            TRAINING_STEPS=20000
         )
         SER[id_SNR] = 1 - Accuracy
 
